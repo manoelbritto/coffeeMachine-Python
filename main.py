56b02356ff0213dd9a6feb574cf8c1d8b576d989
Manoel Britto <manoelbritto@gmail.com>
2023-02-16 22:05:02 -0500

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import coffee_option
 from coffee_option import MENU
 from coffee_option import resources
 
@@ -8,7 +9,7 @@ def report_machine(money):
     print(f"Water: {resources['water']}ml\n"
           f"Milk: {resources['milk']}ml\n"
           f"Coffe: {resources['coffee']}g\n"
-          f"Money: ${money}")
+          f"Money: ${resources['money']}")
 
 
 def check_resources(water, coffee, milk):
@@ -26,13 +27,29 @@ def check_resources(water, coffee, milk):
     return True
 
 
-def process_coin():
+def process_coin(coffee_type):
     """When the user chooses a drink, the program should check if there are enough
     resources to make that drink."""
+    total_value = MENU[coffee_type]["cost"]
+    print(f"coffee_type cost {total_value}")
+    quarters = int(input("How many quarters ($0.25)?"))
+    dimes = int(input("How many dimes ($0.10)?"))
+    nickles = int(input("How many nickles ($0.05)?"))
+    pennies = int(input("How many pennies ($0.01)?"))
+    client_money = (quarters * 0.25) + (dimes*0.10) + (nickles*0.05) + (pennies*0.01)
+    return check_transaction (coffee_type, client_money)
 
-
-def check_transaction():
+def check_transaction(coffee_type, money):
     """Check that the user has inserted enough money to purchase the drink they selected."""
+    total_value = MENU[coffee_type]["cost"]
+    change = money - total_value
+    if change >= 0:
+        print(f"This is your change {round(change,2)}")
+        resources['money'] += total_value
+        return True
+    else:
+        print(f"Not enough money for {coffee_type}, total value {total_value}, but you enter {round(abs(change),2)}")
+        return False
 
 
 def make_coffee(coffee_type):
@@ -44,32 +61,42 @@ def make_coffee(coffee_type):
         water = MENU[coffee_type]["ingredients"]["water"]
         coffee = MENU[coffee_type]["ingredients"]["coffee"]
         if check_resources(water, coffee, milk=0):
-            resources['water'] -= water
-            resources['coffee'] -= coffee
+            if process_coin(coffee_type):
+                resources['water'] -= water
+                resources['coffee'] -= coffee
 
     else:
         water = MENU[coffee_type]["ingredients"]["water"]
         coffee = MENU[coffee_type]["ingredients"]["coffee"]
         milk = MENU[coffee_type]["ingredients"]["milk"]
         if check_resources(water, coffee, milk):
-            resources['water'] -= water
-            resources['coffee'] -= coffee
-            resources['milk'] -= milk
+            if process_coin(coffee_type):
+                resources['water'] -= water
+                resources['coffee'] -= coffee
+                resources['milk'] -= milk
 
 
 def machine_operation():
+    options = ['espresso', 'latte', 'cappuccino', 'report', 'off']
+    right_value = True
     machine_on = True
     money_received = 0.0
     while machine_on:
-        option = input("What would you like? (espresso/latte/cappuccino):").lower()
+        while right_value:
+            option = input("☕ What would you like? (espresso/latte/cappuccino):").lower()
+            if option in options:
+                right_value = False
+            else:
+                print ("Sorry, your have to Type (espresso/latte/cappuccino): ")
         if option == "report":
             report_machine(money_received)
-
+        elif option == 'off':
+                machine_on = False
         else:
             make_coffee(option)
 
-        if option == 'off':
-            machine_on = False
 
+        right_value = True
 
+print (coffee_option.logo)
 machine_operation()
